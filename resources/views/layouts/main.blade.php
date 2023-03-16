500365703c0989f9288498dc34cdbe293da54acb
Dmitry <kor.dima97@mail.ru>
2023-03-16 21:25:34 +0500

    small edit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('build/assets/bootstrap-37892e52.css') }}">
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <title>@yield('title')</title>
 </head>
@@ -17,7 +19,6 @@
 </main>
 
 @include('includes.footer')
-<script src="{{ asset('js/jquery.js') }}"></script>
-<script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
